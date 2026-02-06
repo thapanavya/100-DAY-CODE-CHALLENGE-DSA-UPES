7685a0a8ce00abd39504529650e6bc03f57ff038
Navya Thapa <navyathapa2007@gmail.com>
2026-02-06 22:48:43 +0530

    Add files via upload

---
+++ 006-DAY6/Q11.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];  
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    if (n > 0) {
+        printf("%d ", arr[0]);
+
+        for (i = 1; i < n; i++) {
+            if (arr[i] != arr[i - 1]) {
+                printf("%d ", arr[i]);
+            }
+        }
+    }
+
+    return 0;
+}
+
---
+++ 006-DAY6/Q12.c
@@ -0,0 +1,15 @@
+void moveZeroes(int* nums, int numsSize) {
+    int k = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != 0) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    for (int i = k; i < numsSize; i++) {
+        nums[i] = 0;
+    }
+}
+
