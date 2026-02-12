15adbf3f1b64503d4ed2193fca45723b8ee58e40
Navya Thapa <navyathapa2007@gmail.com>
2026-02-12 22:41:09 +0530

    Add files via upload

---
+++ 012-DAY12/Q13.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    if(m != n) {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                printf("Not a Symmetric Matrix");
+                return 0;
+            }
+        }
+    }
+
+    printf("Symmetric Matrix");
+
+    return 0;
+}
+
---
+++ 012-DAY12/Q14.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+
+bool isToeplitzMatrix(int** matrix, int matrixSize, int* matrixColSize) {
+    int m = matrixSize;
+    int n = matrixColSize[0];
+
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
